9b558f26db8a5803e430dc7b1f6f9df871c0da92
Matteo Lezzi <s270326@studenti.polito.it>
2026-03-24 10:53:49 +0000

    aggiunto file build

---
+++ esercitazione1/build/CMakeFiles/MedieTemp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MedieTemp.dir/link.d"
+  "CMakeFiles/MedieTemp.dir/main.cpp.o"
+  "CMakeFiles/MedieTemp.dir/main.cpp.o.d"
+  "MedieTemp"
+  "MedieTemp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MedieTemp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ esercitazione1/build/CMakeFiles/MedieTemp.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for MedieTemp.
+# This may be replaced when dependencies are built.
